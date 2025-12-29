022ee0150f725d2cb620feb2bcc4f842bf3de418
Suraj Prasad-13 <spyadav0413@gmail.com>
2025-12-29 19:52:06 +0530

    update add banner

--- src/Components/AdsBannerSlider/AdsBannerSlider.tsx
+++ src/Components/AdsBannerSlider/AdsBannerSlider.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import banner1 from "../../assets/banner1.webp";
+import banner2 from "../../assets/banner2.webp";
+import banner3 from "../../assets/banner3.webp";
+import banner4 from "../../assets/banner4.webp";
+import banner5 from "../../assets/banner5.webp";
+import banner6 from "../../assets/banner6.webp";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 
 // import required modules
-import { Navigation } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
+import BannerBox from "../BannerBox/BannerBox";
 const AdsBannerSlider = (props: { items: number }) => {
   return (
     <div className="py-5 w-full">
@@ -14,12 +21,42 @@ const AdsBannerSlider = (props: { items: number }) => {
         slidesPerView={props.items}
         spaceBetween={20}
         navigation={true}
-        modules={[Navigation]}
-        className="mySwiper"
+        loop={true}
+          speed={800}
+         autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+            reverseDirection: false, // force one direction
+          }}
+        modules={[Navigation,Autoplay]}
+        className="smlBtn"
       >
         <SwiperSlide className=" ">
-          <div className="box"></div>
+          <BannerBox img={banner1} link={""} />
         </SwiperSlide>
+        <SwiperSlide className=" ">
+          <BannerBox img={banner2} link={""} />
+        </SwiperSlide>
+        <SwiperSlide className=" ">
+          <BannerBox img={banner3} link={""} />
+        </SwiperSlide>
+        <SwiperSlide className=" ">
+          <BannerBox img={banner4} link={""} />
+        </SwiperSlide>
+        <SwiperSlide className=" ">
+          <BannerBox img={banner5} link={""} />
+        </SwiperSlide>
+        <SwiperSlide className=" ">
+          <BannerBox img={banner6} link={""} />
+        </SwiperSlide>
+        {/* <SwiperSlide className=" ">
+          <BannerBox
+            img={
+              "https://serviceapi.spicezgold.com/download/1760160666204_1737020916820_New_Project_52.jpg"
+            }
+            link={""}
+          />
+        </SwiperSlide> */}
       </Swiper>
     </div>
   );
--- src/Components/BannerBox/BannerBox.tsx
+++ src/Components/BannerBox/BannerBox.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const BannerBox = () => {
+const BannerBox = (props) => {
   return (
-    <div>BannerBox</div>
-  )
-}
+    <div className="box bannerBox rounded-lg overflow-hidden group">
+      <Link to={props.link}>
+        <img src={props.img} className="w-full transition-all group-hover:scale-105 duration-300" alt="banner img" />
+      </Link>
+    </div>
+  );
+};
 
-export default BannerBox
+export default BannerBox;
