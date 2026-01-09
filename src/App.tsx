becc0e2ed77fe03c5bfa027ec7514a78a5213145
Suraj Prasad-13 <spyadav0413@gmail.com>
2026-01-09 12:16:09 +0530

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./Pages/Home/Home";
+import ProductListing from "./Pages/ProductListing/ProductListing";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/productListing" element={<ProductListing />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
